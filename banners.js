00a41f4c5dbff4c8779b4e783096a37d3107062f
Samia Zahan <78074785+SamiaZahan@users.noreply.github.com>
2022-10-12 16:10:14 +0600

    Update banners.js

--- banners.js
+++ banners.js
@@ -52,7 +52,7 @@ const BannerComponent = {
         {
             name: "1",
             //link: "image/banner/cad/large_1.jpg"
-            likn: "https://i.ibb.co/cTDWZVk/Canada-Big-Banner-01.jpg"
+            link: "https://i.ibb.co/cTDWZVk/Canada-Big-Banner-01.jpg"
         },
         
     ],
